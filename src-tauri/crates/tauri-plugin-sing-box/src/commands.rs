861298f1fffe6bfc6618251aa42cd66fe10f7ac8
Tamago <zekun.jin@foxmail.com>
2025-02-13 14:34:11 +0800

    feat: set sys proxy

--- src-tauri/crates/tauri-plugin-sing-box/src/commands.rs
+++ src-tauri/crates/tauri-plugin-sing-box/src/commands.rs
@@ -1,4 +1,5 @@
 use crate::{Error, SingBoxState};
+use rustem_proxy::{Protocol, SystemProxy};
 use tauri::{command, AppHandle, State};
 use tokio::sync::RwLock;
 
@@ -8,10 +9,26 @@ pub async fn start(
     state: State<'_, RwLock<SingBoxState>>,
     config: String,
 ) -> Result<(), Error> {
+    SystemProxy::set(SystemProxy {
+        is_enabled: true,
+        host: "127.0.0.1".to_string(),
+        port: 5129,
+        bypass: "*.local,169.254/16,10.4.38.222,10.255.*,124.220.*".to_string(),
+        protocol: Protocol::SOCKS,
+    });
+
     state.write().await.start(app, config)
 }
 
 #[command]
 pub async fn stop(state: State<'_, RwLock<SingBoxState>>) -> Result<(), Error> {
+    SystemProxy::set(SystemProxy {
+        is_enabled: false,
+        host: "127.0.0.1".to_string(),
+        port: 5129,
+        bypass: "".to_string(),
+        protocol: Protocol::SOCKS,
+    });
+
     state.write().await.stop()
 }
