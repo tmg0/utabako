c2711f6fca0cd81f3dfa4338c98b674b38176d28
Tamago <zekun.jin@foxmail.com>
2025-02-13 15:14:20 +0800

    feat: set system proxy

--- src-tauri/crates/tauri-plugin-sing-box/src/commands.rs
+++ src-tauri/crates/tauri-plugin-sing-box/src/commands.rs
@@ -3,32 +3,47 @@ use rustem_proxy::{Protocol, SystemProxy};
 use tauri::{command, AppHandle, State};
 use tokio::sync::RwLock;
 
+#[cfg(target_os = "windows")]
+static DEFAULT_BYPASS: &str = "localhost;127.*;192.168.*;<local>";
+#[cfg(target_os = "linux")]
+static DEFAULT_BYPASS: &str = "localhost,127.0.0.1,::1";
+#[cfg(target_os = "macos")]
+static DEFAULT_BYPASS: &str = "127.0.0.1,localhost,<local>";
+
+static DEFAULT_HOST: &str = "127.0.0.1";
+
+fn toggle_system_proxy(is_enabled: bool, protocol: String) {
+    let bypass = if is_enabled { DEFAULT_BYPASS } else { "" };
+
+    let protocol = match  protocol.as_str() {
+        "http" => Protocol::HTTP,
+        "https" => Protocol::HTTPS,
+        "socks" => Protocol::SOCKS,
+        _ => Protocol::ALL,
+    };
+
+    SystemProxy::set(SystemProxy {
+        is_enabled: is_enabled,
+        host:DEFAULT_HOST.to_string(),
+        port: 5129,
+        bypass: bypass.to_string(),
+        protocol: protocol,
+    });
+}
+
 #[command]
 pub async fn start(
     app: AppHandle,
     state: State<'_, RwLock<SingBoxState>>,
     config: String,
+    protocol: String
 ) -> Result<(), Error> {
-    SystemProxy::set(SystemProxy {
-        is_enabled: true,
-        host: "127.0.0.1".to_string(),
-        port: 5129,
-        bypass: "*.local,169.254/16,10.4.38.222,10.255.*,124.220.*".to_string(),
-        protocol: Protocol::SOCKS,
-    });
-
+    toggle_system_proxy(true, protocol);
     state.write().await.start(app, config)
 }
 
 #[command]
-pub async fn stop(state: State<'_, RwLock<SingBoxState>>) -> Result<(), Error> {
-    SystemProxy::set(SystemProxy {
-        is_enabled: false,
-        host: "127.0.0.1".to_string(),
-        port: 5129,
-        bypass: "".to_string(),
-        protocol: Protocol::SOCKS,
-    });
-
+pub async fn stop(state: State<'_, RwLock<SingBoxState>>, protocol: String) -> Result<(), Error> {
+    toggle_system_proxy(false, protocol);
     state.write().await.stop()
 }
