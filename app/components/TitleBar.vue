95e42e01b119b5f0bbccf643c4187da5e3bb1174
tmg0 <zekun.jin@foxmail.com>
2025-02-14 20:04:43 +0800

    feat: add win app header

--- app/components/TitleBar.vue
+++ app/components/TitleBar.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
+import { platform } from '@tauri-apps/plugin-os'
+
+const IS_MACOS = platform() === 'macos'
+
 const router = useRouter()
 </script>
 
 <template>
   <div>
-    <div data-tauri-drag-region class="z-50 h-12 bg-white">
-      <div class="h-full flex items-center pl-[72px] justify-between">
-        <Button variant="ghost" size="sm" class="relative z-[101] mx-2 text-xs font-semibold" @click="router.replace({ name: 'index' })">
+    <div data-tauri-drag-region class="z-50 bg-white" :class="[IS_MACOS ? 'h-12' : 'h-8']">
+      <div class="h-full flex items-center justify-between" :class="{ 'pl-[72px]': IS_MACOS }">
+        <Button v-if="IS_MACOS" variant="ghost" size="sm" class="relative z-[101] mx-2 text-xs font-semibold" @click="router.replace({ name: 'index' })">
           UtaBako
         </Button>
+
+        <div v-else class="flex mx-2 gap-1 items-center text-xs">
+          <img src="/images/utabako.png" class="size-5" />
+          <span>UtaBako</span>
+        </div>
       </div>
     </div>
 
